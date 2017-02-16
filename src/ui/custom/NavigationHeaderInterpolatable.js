8e74e107699640a679b46cdcf06d112f7b4ecaae
Wyatt McBain <wyatt.mcbain@zx-ventures.com>
2017-02-16 10:56:37 -0500

    Remove unnecessary title interpolator declaration

--- src/ui/custom/NavigationHeaderInterpolatable.js
+++ src/ui/custom/NavigationHeaderInterpolatable.js
@@ -7,20 +7,13 @@ const { Header: NavigationHeader } = NavigationExperimental
 
 export default class NavigationHeaderInterpolatable extends NavigationHeader {
 
-  _titleInterpolator: (props: NavigationSceneRendererProps) => Object
-
-  constructor(props: Object) {
-    super(props)
-    this._titleInterpolator = props.titleInterpolator
-  }
-
   _renderTitle = (props: NavigationSceneRendererProps): ?React.Element<any> => {
     // $FlowSuppressError
     return this._renderSubView(
       props,
       'title',
       this.props.renderTitleComponent,
-      this._titleInterpolator
+      this.props.titleInterpolator
     )
   }
 }
